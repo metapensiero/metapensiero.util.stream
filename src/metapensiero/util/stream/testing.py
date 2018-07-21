8a12b92f70d79f35622072799a68e69adce4a7d7
Alberto Berti <alberto@metapensiero.it>
2018-07-21 21:44:12 +0200

    Add James' test code, still not functional

--- src/metapensiero/util/stream/testing.py
+++ src/metapensiero/util/stream/testing.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 # :Project:   metapensiero.util.stream -- Some common fixtures and tools
 # :Created:   sab 21 lug 2018 20:42:00 CEST
-# :Author:     <alberto@metapensiero.it>
+# :Author:    Alberto Berti <alberto@metapensiero.it>,
+#             James Stidard <jamesstidard@gmail.com>
 # :License:   GNU General Public License version 3 or later
-# :Copyright: © 2018 Alberto Berti
+# :Copyright: © 2018 Alberto Berti, James Stidard
 #
 
 import asyncio
+from contextlib import contextmanager
+import time
 
 
 async def gen(count, func, delay, initial_delay=None, gen_exc=False):
+    """To be merged with make_async_gen"""
     if initial_delay:
         await asyncio.sleep(initial_delay)
     for i in range(count):
@@ -23,3 +27,31 @@ async def echo_gen():
     v = yield 'initial'
     while v != 'done':
         v = yield v
+
+
+def make_async_gen(yieldables, *, initial_delay: float=0, step_delay: float=0):
+    """
+    Returns async generator yielding with delay.
+
+    Exceptions found in yieldables will be raised.
+    """
+    async def async_gen():
+        await asyncio.sleep(initial_delay)
+        for yieldable in yieldables:
+            await asyncio.sleep(step_delay)
+            if isinstance(yieldable, Exception):
+                raise yieldable
+            else:
+                yield yieldable
+
+    return async_gen
+
+
+@contextmanager
+def profile(*, max_duration: float):
+    """Raises a TimeoutError when enclosed code exceeds duration."""
+    start = time.time()
+    yield
+    actual_duration = time.time() - start
+    if actual_duration > max_duration:
+        raise TimeoutError
--- tests/test_selector.py
+++ tests/test_selector.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 # :Project:   metapensiero.reactive -- selector tests
 # :Created:   lun 30 gen 2017 16:28:18 CET
-# :Author:    Alberto Berti <alberto@metapensiero.it>
+# :Author:    Alberto Berti <alberto@metapensiero.it>,
+#             James Stidard <jamesstidard@gmail.com>
 # :License:   GNU General Public License version 3 or later
-# :Copyright: © 2016, 2017, 2018 Alberto Berti
+# :Copyright: © 2016, 2017, 2018 Alberto Berti, James Stidard
 #
 
-import asyncio
 from functools import partial
 
 import pytest
 
 from metapensiero.util.stream import Selector
-from metapensiero.util.stream.testing import gen, echo_gen
+from metapensiero.util.stream.testing import (
+    gen, echo_gen, make_async_gen, profile)
 
 
 @pytest.mark.asyncio
@@ -84,3 +85,70 @@ async def test_selector_send(event_loop):
         data.append(v)
 
     assert data == ['initial', 1, 'a', 1, 'a']
+
+
+@pytest.mark.asyncio
+async def test_selector_order():
+    source_1 = make_async_gen([1, 2, 3], step_delay=0.2)
+    source_2 = make_async_gen([4, 5, 6], initial_delay=0.1, step_delay=0.2)
+    expected = [1, 4, 2, 5, 3, 6]
+
+    async for value in Selector(source_1(), source_2()):
+        assert value == expected.pop(0)
+
+
+@pytest.mark.asyncio
+@pytest.mark.timeout()
+async def test_selector_blocking():
+    source_1 = make_async_gen([1, 2, 3], step_delay=1)
+    source_2 = make_async_gen([1, 2, 3], step_delay=1)
+
+    with profile(max_duration=3.1):
+        async for _ in Selector(source_1(), source_2()):
+            pass
+
+
+@pytest.mark.asyncio
+async def test_yielded_source_with_values():
+    source_values = [
+        [object(), object(), object()],
+        [object(), object(), object()],
+        [object(), object(), object()],
+    ]
+    source_values = {make_async_gen(vs)(): vs for vs in source_values}
+
+    # Note: seems like a code smell for a param, like return_source, to change the output.
+    # Might make more sense to attach to a method like dict.items() or just always return
+    # both and expect some _, value type unpacking.
+    async for source, value in Selector(*source_values.keys(), return_source=True):
+        source_values[source].remove(value)
+    else:
+        assert all(len(vs) == 0 for vs in source_values.values())
+
+
+@pytest.mark.asyncio
+async def test_exception_handling():
+    source_1 = make_async_gen([0, 1, 2, 3, 4, Exception()])
+    source_2 = make_async_gen([0, 1, Exception(), 3, 4, 5])
+    expected = [0, 0, 1, 1, 2, Exception, 3, 4, Exception]
+
+    # Note: same kind of consideration as above about a param changing
+    # the output type.
+    # Also, asyncio.wait you can await and it'll return a Future to be
+    # awaited to handle
+    # exceptions raised. It would mean Selector opening the result and
+    # then rewrapping the result/exception in a Future to be returned
+    # to the caller.
+    async for value in Selector(source_1(), source_2()):
+        # noinspection PyBroadException
+        try:
+            # maybe yieldable returned
+            value = yield value
+            # or maybe awaitable
+            # value = await value
+            # or future
+            # value = await value.result()
+        except Exception:
+            assert isinstance(expected.pop(0), Exception)
+        else:
+            assert value == expected.pop(0)
